7f6ab7a60a61c8bbb6525ec0dd02e7f007eb66e6
alhans2708 <alhans2708@gmail.com>
2024-01-04 11:49:58 +0700

    [FEATURE] Koneksi Database.

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"enigma-laundry/entity"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	pass   = "7678473"
+	dbname = "enigma_laundry"
+)
+
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, pass, dbname)
 
 func main() {
 	customer := entity.Customer{Id: 1, Name: "Jessica", Phone: "089765456324", Address: "Jakarta"}
@@ -12,4 +25,25 @@ func main() {
 	fmt.Println(customer)
 	fmt.Println(employer)
 	fmt.Println(layanan)
+
+	fmt.Println(connectDb())
+}
+
+func connectDb() *sql.DB {
+	db, err := sql.Open("postgres", psqlInfo)
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Successfully Connected!")
+	}
+
+	return db
 }
